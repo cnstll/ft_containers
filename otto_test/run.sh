0128870f2ef242fc182cee2c8411e26911222cff
cnstll <calle@student.42.fr>
2022-02-07 19:10:20 +0000

    Welcome Otto ! automating testing for vectors

---
+++ otto_test/run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+source sub_scripts/colors.sh
+
+ROOT_DIR="../"
+TESTED_CONTAINER="vector"
+DIFF_FILE="diff_${TESTED_CONTAINER}.log"
+
+rm -f ${DIFF_FILE}
+rm -f your_${TESTED_CONTAINER}_output.log your_${TESTED_CONTAINER} stl_${TESTED_CONTAINER}_output.log stl_${TESTED_CONTAINER}
+
+cp ./${ROOT_DIR}${TESTED_CONTAINER}.hpp .
+cp ./${ROOT_DIR}test_${TESTED_CONTAINER}.cpp .
+
+c++ -DACTIVE_NAMESPACE="std" -g -fsanitize=address -Wall -Wextra -Werror test_${TESTED_CONTAINER}.cpp -o stl_vector
+if [ $? != 0 ]
+then   
+    echo -e "${RED} >> COMPILATION ERROR ${RESET}"
+    exit 1
+fi
+c++ -DACTIVE_NAMESPACE="ft" -g -fsanitize=address -Wall -Wextra -Werror test_${TESTED_CONTAINER}.cpp -o your_vector
+if [ $? != 0 ]
+then
+    echo -e "${RED} >> COMPILATION ERROR ${RESET}"
+    exit 1
+fi
+
+echo -e "${YELLOW}STDERROR OUTPUT...${RESET}"
+echo -e "${YELLOW}STL STDERROR: ${RESET}"
+./stl_vector > stl_${TESTED_CONTAINER}_output.log
+echo -e "${YELLOW}YOUR STDERROR: ${RESET}"
+./your_vector > your_${TESTED_CONTAINER}_output.log
+
+echo
+diff  stl_${TESTED_CONTAINER}_output.log your_${TESTED_CONTAINER}_output.log > ${DIFF_FILE}
+
+if [ -s "$DIFF_FILE" ]
+then
+    echo -e "${RED} ✖ AT LEAST 1 TEST FAILED - CHECK DIFF${RESET}"
+else
+    echo -e "${GREEN} ✔ TESTS PASSED${RESET}"
+    rm -f ${DIFF_FILE}
+fi
+
+rm -f your_${TESTED_CONTAINER} stl_${TESTED_CONTAINER}
+#rm -f your_${TESTED_CONTAINER}_output.log stl_${TESTED_CONTAINER}_output.log
+rm -f *.hpp *.cpp
