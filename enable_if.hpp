b077c36b94f4ff1f6245aa8e3185727f4d8f8c5b
calle <calle@student.42.fr>
2022-02-14 19:06:34 +0100

    enable if base struct

--- enable_if.hpp
+++ enable_if.hpp
@@ -2,11 +2,19 @@
 #define ENABLE_IF_HPP
 
 namespace ft {
+/**
+ * Triggers SFINAE: Substitution Failure Is Not An Error
+ */
+template <bool Cond, class T = void> struct enable_if 
+{
+};
 
-template <bool Cond, class T = void> struct enable_if{
+template<typename T> struct enable_if<true, T> {
 
   typedef T type;
-};
+
+}
+//https://www.fluentcpp.com/2018/05/15/make-sfinae-pretty-1-what-value-sfinae-brings-to-code/
 
 }; //namespace
 #endif
