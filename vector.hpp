7cfb03ddaba3212ed83ad5fe8b16931cc2e464f9
cnstll <calle@student.42.fr>
2022-02-18 17:39:29 +0000

    implementing reverse iterator and const rev iterator, rend and rbegin

--- vector.hpp
+++ vector.hpp
@@ -17,29 +17,22 @@ namespace ft {
 
 // ! TODO: Rewrite specs for vector  - atm its stack specs
 /**
- *  The std::stack class is a container adaptor that gives the programmer the functionality of a stack - specifically,
- *  a LIFO (last-in, first-out) data structure.
- *  The class template acts as a wrapper to the underlying container - only a specific set of functions is provided.
- *  The stack pushes and pops the element from the back of the underlying container, known as the top of the stack.
- *  source: https://en.cppreference.com/w/cpp/container/stack
-*/
+ * @brief The elements are stored contiguously, which means that elements can be accessed not only through iterators, 
+ *        but also using offsets to regular pointers to elements. 
+ *        This means that a pointer to an element of a vector may be passed to any function that expects a pointer to an element of an array.
+ *        The storage of the vector is handled automatically, being expanded and contracted as needed.
+ *        Vectors usually occupy more space than static arrays, because more memory is allocated to handle future growth.
+ *        source: https://en.cppreference.com/w/cpp/container/vector
+ */
 
 /**
  * 
- * @param T The type of the stored elements. The behavior is undefined if T is not the same type as Container::value_type.
- * @param Container The type of the underlying container to use to store the elements.
- * The container must satisfy the requirements of SequenceContainer.
- * Additionally, it must provide the following functions with the usual semantics:
- *     - back()
- *     - push_back()
- *     - pop_back()
- * Here, by default vector is used if no contianer is specified.
+ * @param T The type of the stored elements.
+ * @param Allocator An allocator that is used to acquire/release memory and to construct/destroy the elements in that memory.
  */
 
-template<
-    class T,
-    class Allocator = std::allocator<T>
-> class vector {
+template< class T, class Allocator = std::allocator<T>>
+class vector {
 
     public:
         /**
@@ -55,19 +48,25 @@ template<
         typedef const value_type& const_reference;
         typedef typename ft::vectorIterator<T> iterator;
         typedef typename ft::constVectorIterator<T> const_iterator;
-        //typedef reverse_iterator;
-        //typedef const_reverse_iterator;
+        typedef typename ft::reverse_iterator<vectorIterator<T>> reverse_iterator;
+        typedef typename ft::reverse_iterator<constVectorIterator<T>> const_reverse_iterator;
 
         /**
          * Member functions
         */
-        // Default constructor. Constructs an empty container with a default-constructed allocator.
-        vector(void) : currentSize(0), currentCapacity(0), n(NULL) {};
+        /**
+         * @brief Default constructor. Constructs an empty container with a default-constructed allocator.
+         */
+        vector() : currentSize(0), currentCapacity(0), n() {};
 
-        //Constructs an empty container with the given allocator alloc.
+        /**
+         * @brief Constructs an empty container with the given allocator alloc.
+         */
         explicit vector(const Allocator &alloc)
-            : n_allocator(alloc), currentSize(0), currentCapacity(0) {};
-        // Constructs the container with count copies of elements with value value.
+            : n_allocator(alloc), currentSize(0), currentCapacity(0), n(){};
+        /**
+         * @brief  Constructs the container with count copies of elements with value value.
+         */
         explicit vector(size_type count, const T &value = T(), const Allocator &alloc = Allocator())
             : n_allocator(alloc), currentSize(count), currentCapacity(count) {
 
@@ -77,7 +76,9 @@ template<
                 //n[i] = value;
             }
         };
-        // Constructs the container with the contents of the range [first, last)
+        /**
+         * @brief Constructs the container with the contents of the range [first, last) 
+         */
         template <class InputIt>
         vector(InputIt first, InputIt last, const Allocator &alloc = Allocator(),
         typename ft::enable_if<!ft::is_integral<InputIt>::value>::type* = 0) {
@@ -99,9 +100,24 @@ template<
                 ++i;
             }
         };
-        // Copy Construct
+        /**
+         * @brief Copy Construct
+         */
         vector(const vector &other) { *this = other; };
 
+        /**
+         * @brief  Destructs the vector. The destructors of the elements are called and the used storage is deallocated.
+         *         Note, that if the elements are pointers, the pointed-to objects are not destroyed.
+         *
+         */
+        ~vector(){
+            n_allocator.destroy(n);
+            if (n && capacity() > 0)
+                n_allocator.deallocate(n, capacity()); 
+        };
+    /*
+     * Functions needed for deep copying in operator=
+     */    
     private:
 
         void    copyElements(T *dest, T *src, size_type size) {
@@ -125,27 +141,24 @@ template<
         };
 
     public:
-
+        /**
+         * @brief Assigns values to the container by deep copying it
+         */
         vector &operator=(const vector &other) {
             if (this != &other)
                 deepCopy(other);
             return *this;
         };
-        // Destructs the vector. The destructors of the elements are called and the used storage is deallocated.
-        // Note, that if the elements are pointers, the pointed-to objects are not destroyed.
-        ~vector(){
-            //for (size_type i = 0; i < currentSize; i++){
-            //    n_allocator.destroy(&n[i]);
-            //}
-            //std::cout << "Destructor\n";
-            if (n && capacity() > 0)
-                n_allocator.deallocate(n, capacity()); 
-        };
+
 
         /**
          * Element access 
         */
-        //Returns a reference to the element at specified location pos. Bounds checking is performed.
+        
+        /**
+         * @brief Returns a reference to the element at specified location pos.
+         *        Bounds checking is performed.
+         */
         reference at( size_type pos ){
             if (!(pos < size())){
                 std::stringstream sstm_err;
@@ -167,16 +180,23 @@ template<
             return (*this)[pos];
         };
 
-        //Returns a reference to the element at specified location pos. No bounds checking is performed.
+        /**
+         * @brief Returns a reference to the element at specified location pos. No bounds checking is performed. 
+         */
         reference operator[]( size_type pos ){ return *(n + pos); };
         const_reference operator[]( size_type pos ) const { return *(n + pos); };
 
-        // ! DO CONST ITERATORS
-        // Returns a reference to the first element in the container. Calling front on an empty container is undefined.
+        /**
+         * @brief Returns a reference to the first element in the container.
+         *        Calling front on an empty container is undefined.
+         */
         reference front() { return *begin(); };
         const_reference front() const { return *begin(); };
 
-        // Returns a reference to the last element in the container.
+        /**
+         * @brief Returns a reference to the last element in the container.
+         *        Calling back on an empty container is undefined.
+         */
         reference back() { return *(end() - 1); };
         const_reference back() const { return *(end() - 1); };
 
@@ -184,43 +204,71 @@ template<
         // The pointer is such that range [data(); data() + size()) is always a valid range, even if the container is empty (data() is not dereferenceable in that case).
         T* data() { return this->n; };
         const T* data() const { return this->n; };
-
-        //Replaces the contents with count copies of value value
+        //! ASSIGN TO DO
+        /**
+         * @brief Replaces the contents with count copies of value value
+         */
         //void assign( size_type count, const T& value ){};
-        //Replaces the contents with copies of those in the range [first, last). The behavior is undefined if either argument is an iterator into *this.
+        /**
+         * @brief Replaces the contents with copies of those in the range [first, last).
+         * The behavior is undefined if either argument is an iterator into *this.
+         */
         //template< class InputIt > void assign( InputIt first, InputIt last );
 
-        //Returns the allocator associated with the container.
+        /**
+         * @brief Returns the allocator associated with the container.
+         */
         allocator_type get_allocator() const { return this->n_allocator; };
+
         /**
          * Iterators 
         */
-
-        // ! DO REVERSE
         iterator begin() { iterator it = n; return it; };
         const_iterator begin() const { const_iterator it = n; return it; };
-//
+        /**
+         * @brief Returns an iterator to the element following the last element of the vector.
+         *        This element acts as a placeholder; attempting to access it results in undefined behavior.
+         */
         iterator end() { iterator it = (n + size()); return it; };
-        const iterator end() const { const iterator it = (n + size()); return it; };
-        //reverse_iterator rbegin() { reverse_iterator it = (n + size); return it };
-        //const_reverse_iterator rbegin() { const_reverse_iterator it = (n + size); return it };
-        //reverse_iterator rend() { const_reverse_iterator it = n; return it; };
-        //const_reverse_iterator rend() { const_reverse_iterator it = n; return it; };
+        const_iterator end() const { const_iterator it = (n + size()); return it; };
+        
+        reverse_iterator rbegin() { reverse_iterator rit(end()); return rit; };
+        const_reverse_iterator rbegin() const { const_reverse_iterator rit(end()); return rit; };
+
+        /**
+         * @brief Returns a reverse iterator to the element following the last element of the reversed vector.
+         *        It corresponds to the element preceding the first element of the non-reversed vector.
+         *        This element acts as a placeholder, attempting to access it results in undefined behavior.
+         */
+        reverse_iterator rend() { reverse_iterator rit(begin()); return rit; };
+        const_reverse_iterator rend() const { const_reverse_iterator rit(begin()); return rit; };
+        
         /**
          * Capacity 
         */
+        
+        /**
+         * @brief check wether the container is empty
+         */
         bool empty() const { return (currentSize == 0); };
-        // Returns the number of elements in the container, i.e. std::distance(begin(), end())
+        /**
+         * @brief   Returns the number of elements in the container, i.e. the distance between (begin(), end()).
+         */
         size_type size() const { return currentSize; };
 
-        // Returns the maximum number of elements the container is able to hold due to system or library implementation limitations,
-        // i.e. std::distance(begin(), end()) for the largest container.
+        /**
+         * @brief Returns the maximum number of elements the container is able to hold due to system or library implementation limitations.
+         */
         size_type max_size() const { return n_allocator.max_size(); };
 
-        size_type capacity() const { return currentCapacity;};
         /**
-         * Increase the capacity of the vector to a value that's greater or equal to new_cap.
-         * If new_cap is greater than the current capacity(), new storage is allocated, otherwise the function does nothing.
+         * @brief returns the number of elements that can be held in currently allocated storage
+         */
+        size_type capacity() const { return currentCapacity; };
+
+        /**
+         * @brief Increase the capacity of the vector to a value that's greater or equal to new_cap.
+         *        If new_cap is greater than the current capacity(), new storage is allocated, otherwise the function does nothing.
          */
         void reserve( size_type new_cap ){
             if (new_cap > max_size())
@@ -236,9 +284,16 @@ template<
                 catch(...) {};
             }
         };
+
         /**
          * Modifiers 
         */
+        
+        /**
+         * @brief Erases all elements from the container. After this call, size() returns zero.
+         *        Invalidates any references, pointers, or iterators referring to contained elements.
+         *        Any past-the-end iterators are also invalidated. 
+         */
         void clear(){
 
             while (0 < currentSize) {
@@ -246,17 +301,12 @@ template<
             }
             currentSize = 0;
         };
+
     private:
-        void    ctorElements(T *dest, iterator start, iterator end) {
-
-            iterator it = start;
-            while (it != end) {
-                n_allocator.construct(dest, *it);
-                dest++;
-                it++;
-            }
-        };
-
+        
+        /**
+         * @brief Measure the distance between the two pointers passed as arguments.
+         */
         template <typename InputIt>
         size_type _distance(InputIt start, InputIt end) {
            size_type distance = 0;
@@ -271,11 +321,11 @@ template<
 
     public:
         /**
-         * Inserts elements at the specified location in the container.
-         * Causes reallocation if the new size() is greater than the old capacity().
-         * If the new size() is greater than capacity(), all iterators and references are invalidated.
-         * Otherwise, only the iterators and references before the insertion point remain valid.
-         * The past-the-end iterator is also invalidated.
+         * @brief Inserts elements at the specified location in the container.
+         *        Causes reallocation if the new size() is greater than the old capacity().
+         *        If the new size() is greater than capacity(), all iterators and references are invalidated.
+         *        Otherwise, only the iterators and references before the insertion point remain valid.
+         *        The past-the-end iterator is also invalidated.
          */
         iterator insert( iterator pos, const T& value ) {
             size_type d1 = _distance<iterator>(begin(), pos);
@@ -356,12 +406,8 @@ template<
         };
 
         /**
-         * Removes the element at pos.
-         * Removes the elements in the range [first, last).
-         * Invalidates iterators and references at or after the point of the erase, including the end() iterator.
-         * The iterator pos must be valid and dereferenceable. 
-         * Thus the end() iterator (which is valid, but is not dereferenceable) cannot be used as a value for pos.
-         * The iterator first does not need to be dereferenceable if first==last: erasing an empty range is a no-op.
+         * @brief Removes the element at pos.
+         * @return Iterator following the last removed element.
          */
         iterator erase( iterator pos ) {
 
@@ -378,7 +424,13 @@ template<
             else
                 return pos;
         };
-
+        /**
+         * @brief Removes the elements in the range [first, last).
+         * Invalidates iterators and references at or after the point of the erase, including the end() iterator.
+         * The iterator pos must be valid and dereferenceable. 
+         * Thus the end() iterator (which is valid, but is not dereferenceable) cannot be used as a value for pos.
+         * The iterator first does not need to be dereferenceable if first==last: erasing an empty range is a no-op.
+         */
         iterator erase( iterator first, iterator last ) {
 
             size_type d_range = _distance<iterator>(first, last);
@@ -476,15 +528,15 @@ template<
         //template <class _T, class _Container>
         //friend bool operator==(const ft::vector<_T, _Container> &lhs, const ft::vector<_T, _Container> &rhs);
 };
-// TODO: look into EqualityComparable requirement https://en.cppreference.com/w/cpp/named_req/EqualityComparable
+
 /**
- * Non member functions
+ * Vector Non member functions
 */
 
 // Operator "<" and "==" are used to build the other operator overloads to limit the use of "friend" functions
 template< class T, class Container >
     bool operator<( const ft::vector<T,Container>& lhs, const ft::vector<T,Container>& rhs ) {
-        return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), lhs.end()); 
+        return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()); 
     };
 
 template< class T, class Container >
@@ -504,6 +556,6 @@ template< class T, class Container >
 
 template< class _T, class _Alloc >
 void swap( ft::vector<_T, _Alloc>& lhs, ft::vector<_T, _Alloc>& rhs ){ lhs.swap(rhs); };
-}; // namespace
 
+}; // namespace
 #endif
