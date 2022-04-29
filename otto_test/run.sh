e8964bb00540c85b410e75b91574afacfdf03b9f
cnstll <calle@student.42.fr>
2022-04-29 12:21:58 +0200

    Otto now display time + formating of msg | putting notes in readme

--- otto_test/run.sh
+++ otto_test/run.sh
@@ -42,7 +42,7 @@ rm -f ${COMPILATION_ERROR_FILE} ${EXECUTION_ERROR_FILE}
 if [[ ${TESTED_CONTAINER} != "vector" ]] && [[ ${TESTED_CONTAINER} != "map" ]] && [[ ${TESTED_CONTAINER} != "stack" ]]
 then    
     echo -e "ERROR: unkown argument"
-    echo -e "USAGE: ./run.sh [vector|map]"
+    echo -e "USAGE: ./run.sh [stack|vector|map]"
     exit 1
 fi
 # Creating tests folder
@@ -78,7 +78,7 @@ for test in ./${TESTED_FILES_FOLDER}${TESTED_CONTAINER}_testing/*.cpp; do
 
     COUNT_TOTAL_TESTS=$((COUNT_TOTAL_TESTS + 1))
 	test_name=$(basename ${test} | sed "s'\.cpp''")
-    echo -en "TEST #${COUNT_TOTAL_TESTS}: ${test_name} - "
+    echo -en "${YELLOW}> TEST #${COUNT_TOTAL_TESTS}: ${test_name}${RESET} - "
     c++ ${COMPILATION_FLAGS} ${test} -o ${STL_BIN} -D NAMESPACE="std"
     c++ ${COMPILATION_FLAGS} ${test} -o ${YOUR_BIN} -D NAMESPACE="ft" 2>> ${COMPILATION_ERROR_FILE}
     if [ $? -eq 0 ]
@@ -89,30 +89,42 @@ for test in ./${TESTED_FILES_FOLDER}${TESTED_CONTAINER}_testing/*.cpp; do
         echo -e "EXECUTION: ${RED}ERROR${RESET}"
         continue
     fi
-    STL_START=$(date +.%N)
-    ./${STL_BIN} > "${STL_OUTPUT_FOLDER}${STL_OUTPUT}_${test_name}.log" 2> /dev/null
-    STL_END=$(date +.%N)
-    YOURS_START=$(date +.%N)
-    ./${YOUR_BIN} > "${YOUR_OUTPUT_FOLDER}${YOUR_OUTPUT}_${test_name}.log" 2>> ${EXECUTION_ERROR_FILE}
-    EXEC_RET=$?
-    YOURS_END=$(date +.%N)
+    # STL_START=$(date +.%N)
+    if [[ $2 ]] && [[ $2 == "time" ]]
+    then
+        echo -ne "\nSTL TIMES: "
+        time ./${STL_BIN} > "${STL_OUTPUT_FOLDER}${STL_OUTPUT}_${test_name}.log" 2> /dev/null
+        # STL_END=$(date +.%N)
+        # YOURS_START=$(date +.%N)
+        echo -e "------------------"
+        echo -ne "YOUR TIMES: "
+        time ./${YOUR_BIN} > "${YOUR_OUTPUT_FOLDER}${YOUR_OUTPUT}_${test_name}.log" 2>> ${EXECUTION_ERROR_FILE}
+        EXEC_RET=$?
+    elif [[ $2 ]] && [[ $2 != "time" ]]
+    then
+            echo -e "ERROR: unkown argument"
+            echo -e "USAGE: ./run.sh [stack|vector|map]"
+            exit 1
+    else
+        ./${STL_BIN} > "${STL_OUTPUT_FOLDER}${STL_OUTPUT}_${test_name}.log" 2> /dev/null
+        ./${YOUR_BIN} > "${YOUR_OUTPUT_FOLDER}${YOUR_OUTPUT}_${test_name}.log" 2>> ${EXECUTION_ERROR_FILE}
+        EXEC_RET=$?
+    fi
+    # YOURS_END=$(date +.%N)
     if [ ${EXEC_RET} -ne 0 ]
     then
         echo -e "EXECUTION: ${RED}ERROR${RESET}"
-        if [ $2 ] && [ $2 -eq 0 ]
-        then    break
-        fi
     else
         diff  ${STL_OUTPUT_FOLDER}${STL_OUTPUT}_${test_name}.log ${YOUR_OUTPUT_FOLDER}${YOUR_OUTPUT}_${test_name}.log > ${DIFF_FOLDER}${DIFF_FILE}_${test_name}
         if [ -s "${DIFF_FOLDER}${DIFF_FILE}_${test_name}" ]
         then
             echo -e "EXECUTION: ${RED}FAILED${RESET}"
         else
-            echo -ne "EXECUTION: ${GREEN}SUCCESS${RESET} - "
-            STL_DIFF=$(echo "$STL_END - $STL_START" | bc -l)
-            YOURS_DIFF=$(echo "$YOURS_END - $YOURS_START" | bc -l)
-            EXEC_TIME_MULTIPLE=$(echo "$STL_DIFF/$YOURS_DIFF" | bc -l)
-            echo -e ""STL EXEC TIME / YOUR EXEC TIME": ${EXEC_TIME_MULTIPLE}"
+            echo -e "EXECUTION: ${GREEN}SUCCESS${RESET}"
+            # STL_DIFF=$(echo "$STL_END - $STL_START" | bc -l)
+            # YOURS_DIFF=$(echo "$YOURS_END - $YOURS_START" | bc -l)
+            # EXEC_TIME_MULTIPLE=$(echo "$STL_DIFF/$YOURS_DIFF" | bc -l)
+            # echo -e ""STL EXEC TIME / YOUR EXEC TIME": ${EXEC_TIME_MULTIPLE}"
             COUNT_PASSED_TESTS=$((COUNT_PASSED_TESTS + 1))
             rm -f ${DIFF_FOLDER}${DIFF_FILE}_${test_name}
         fi
